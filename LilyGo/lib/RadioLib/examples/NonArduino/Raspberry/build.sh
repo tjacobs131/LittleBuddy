86c1096ada1234d6aafcbfe918c0cdc05b016e3b
tjacobs131 <tomjacobs2003@hotmail.com>
2024-09-12 14:33:23 +0200

    Fix gitignore

--- LilyGo/lib/RadioLib/examples/NonArduino/Raspberry/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-mkdir -p build
-cd build
-cmake -G "CodeBlocks - Unix Makefiles" ..
-make
-cd ..
-size build/rpi-sx1261
--- LilyGo/lib/RadioLib/src/modules/LR11x0/LR1121.cpp
+++
@@ -1,8 +0,0 @@
-#include "LR1121.h"
-#if !RADIOLIB_EXCLUDE_LR11X0
-
-LR1121::LR1121(Module* mod) : LR1120(mod) {
-  chipType = RADIOLIB_LR11X0_DEVICE_LR1121;
-}
-
-#endif
