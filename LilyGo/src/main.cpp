d21247e89b7fce0764e8b775ad4e102128b57ece
Koen van Wijlick <koenvanwijlick@gmail.com>
2024-10-07 12:51:09 +0200

    Addition of test file of buzzer

--- LilyGo/src/main.cpp
+++ LilyGo/src/main.cpp
@@ -1,14 +1,17 @@
-#include <SPI.h>
-#include <LoRa.h>
+#include "Buzzer.h"
+
+Buzzer buzzer(9);  // Buzzer connected to pin 9
 
 void setup() {
-  Serial.begin(9600);
-  LoRa.begin(868E6); // Set the frequency (e.g., 868 MHz for Europe)
+    buzzer.begin();
+    buzzer.playHappyTone();  // Play a happy tone on startup
 }
 
 void loop() {
-  LoRa.beginPacket();
-  LoRa.print("Mock data from TTGO");
-  LoRa.endPacket();
-  delay(5000); // Send data every 5 seconds
+    // Example of how to play different tones based on some condition
+    buzzer.playWarningTone();  // Play a warning sound
+    delay(2000);  // Wait for 2 seconds
+    
+    buzzer.playErrorTone();  // Play an error sound
+    delay(3000);  // Wait for 3 seconds
 }
