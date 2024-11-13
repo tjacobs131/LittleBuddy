2c9a204f7b51a82ea6ad4f593f80540126d0457c
JamesBoogaard <93592094+JamesBoogaard@users.noreply.github.com>
2024-11-13 15:03:53 +0100

    test for tom

---
+++ Server/stuff/Program.cs
@@ -0,0 +1,30 @@
+using SignalRChat.Hubs;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddSignalR();
+builder.Services.AddSingleton<RandomNumberService>();
+
+var app = builder.Build();
+
+app.MapControllers();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapHub<ChatHub>("/chatHub");
+
+app.Run();
+
